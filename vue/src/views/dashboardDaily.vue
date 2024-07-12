847e295d577cf5b8c40d4a23da172ec0beb8700d
Jin Yun Lin <yunlin.jin.2022@scis.smu.edu.sg>
2024-07-13 02:11:59 +0800

    updated tableau

--- vue/src/views/dashboardDaily.vue
+++ vue/src/views/dashboardDaily.vue
@@ -2,7 +2,7 @@
     <body>
         <div class="tableau-container">
         <tableau-viz id='tableau-viz' 
-        src='https://prod-apnortheast-a.online.tableau.com/t/yunlinjin20227e130be587/views/Dashboard_v1_4/TotalSpentbyDay' 
+        src='https://public.tableau.com/views/Daily_17208076795010/TotalSpentbyDay?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link' 
         width='1920' height='905' toolbar='bottom' ></tableau-viz>
         </div>
 
